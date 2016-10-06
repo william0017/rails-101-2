07a162007461ebbdcc89039277ec99cd8f1eb471
william0017E <wangwei0017@gmail.com>
2016-10-06 14:45:09 +0800

    Final

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,7 +6,9 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
 
-    has_many :group_relationships
+  has_many :groups
+  has_many :posts
+  has_many :group_relationships
   has_many :participated_groups, :through => :group_relationships, :source => :group
 
   def is_member_of?(group)
